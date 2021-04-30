5a133b528df760ec88c602891f2ed4324a65f678
Shawn <alsdud110@naver.com>
2021-04-30 13:58:08 +0900

    1

--- ShoppingMall/src/main/java/config/ControllerConfig.java
+++ ShoppingMall/src/main/java/config/ControllerConfig.java
@@ -68,12 +68,12 @@ public class ControllerConfig {
 		return controller;
 	}
 	
-		@Bean
-		   public MainController mainController() { //최고 판마량 제품 보여주기위해 productService 추가
-		      MainController controller = new MainController();
-		      controller.setProductService(productService);
-		      return controller;
-		   }
+	@Bean
+	public MainController mainController() { //최고 판마량 제품 보여주기위해 productService 추가
+		MainController controller = new MainController();
+		controller.setProductService(productService);
+		return controller;
+	}
 	
 
 	@Bean
